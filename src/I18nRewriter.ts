77c7565933baec492bfc747f103c75d1f8c95714
Brad <broswen@gmail.com>
2023-10-15 12:50:26 -0500

    initial commit

---
+++ src/I18nRewriter.ts
@@ -0,0 +1,48 @@
+
+export type TranslationValue = string | (() => string) | number | undefined
+export interface TranslationContext {
+	[key: string]: TranslationValue
+}
+export interface TranslationMap {
+	[key: string]: TranslationContext
+}
+
+export class I18nRewriter implements HTMLRewriterElementContentHandlers {
+	context: TranslationContext | undefined
+	constructor(context: TranslationContext | undefined) {
+		this.context = context
+	}
+
+	element(element: Element): void | Promise<void> {
+		if (!this.context) return;
+
+		// get translation key and remove the attribute
+		const key = element.getAttribute("i18n");
+		element.removeAttribute("i18n");
+
+		// skip element if attribute value doesn't exist for some reason
+		if (!key) return;
+
+		// get the value for the translation key in the current context
+		const value = this.context[key];
+
+		// skip element if key doesn't exist in translation context
+		if (!value) return;
+
+		// handle more underlying translation types here
+		switch (typeof value) {
+			case "string":
+				element.setInnerContent(value);
+				break;
+			case "number":
+				// TODO format number based on locale?
+				element.setInnerContent(`${value}`);
+				break;
+			case "function":
+				element.setInnerContent(value())
+				break;
+			default:
+				element.setInnerContent(`${value}`)
+		}
+	}
+}
---
+++ src/index.ts
@@ -0,0 +1,66 @@
+import template from "./template.html";
+import { I18nRewriter, TranslationMap } from './I18nRewriter';
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export interface Env {
+	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+	// MY_KV_NAMESPACE: KVNamespace;
+	//
+	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+	// MY_DURABLE_OBJECT: DurableObjectNamespace;
+	//
+	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+	// MY_BUCKET: R2Bucket;
+	//
+	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
+	// MY_SERVICE: Fetcher;
+	//
+	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
+	// MY_QUEUE: Queue;
+}
+
+const TRANSLATION_MAP: TranslationMap = {
+	"en-us": {
+		"title": "Hello World!",
+		"title.description": "This is a test translation.",
+		"number": 3.1415,
+		"date": () => new Date().toLocaleDateString("en-us")
+		},
+	"es-es": {
+		"title": "Hola Mundo!",
+		"title.description": "Esta es una traducción de prueba.",
+		"number": 3.1415,
+		"date": () => new Date().toLocaleDateString("es-es")
+	}
+}
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const url = new URL(request.url)
+
+		// get language code from HTTP Accept-Language header
+		// TODO make this more robust and handle multiple preferences
+		const acceptLanguage = (request.headers.get("Accept-Language") ?? "en-us").split(",")[0].toLowerCase();
+
+		// create response with template and set Content-Language with the language chosen
+		let res = new Response(template, {
+			headers:{
+				"Content-Type": "text/html",
+				"Content-Language": acceptLanguage
+			}
+		});
+    // create i18n rewriter with translation context
+		const rewriter = new HTMLRewriter().on("*[i18n]", new I18nRewriter(TRANSLATION_MAP[acceptLanguage]))
+
+		// return the response while transforming the content
+		return rewriter.transform(res)
+	},
+};
